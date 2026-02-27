ab6d5e5248c7c768c93a23ab97532e90ef559f27
Theerthananda <theerthanandagowda@gmail.com>
2026-02-27 23:19:47 +0530

    MENU DRIVEN PROGRAM OF QUEUE IS ADDED

--- STACK AND QUEUS/LinaerQueue.c
+++ STACK AND QUEUS/LinaerQueue.c
@@ -53,10 +53,10 @@ int main(){
 	int ch;
 	int x;
 	
-	/*while(1){
+	while(1){
 		printf("\nMENU\n1.INSERT\n2.DELETE\n3.DISPLAY\n4.EXIT\nENTER YOUR CHOICE (1-4) :");
 		scanf("%d",&ch);
-		*/
+		
 		switch(ch){
 			case 1:
 				printf("\nEnter value to insert :");
@@ -79,6 +79,6 @@ int main(){
 						default:
 						printf("\nEnter vallid choice !\n");		
 		}
-	//}
+	}
 	return 0;
 }
