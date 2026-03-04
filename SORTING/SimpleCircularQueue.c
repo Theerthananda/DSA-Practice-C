9f2f6cc5155bb8306d333d2f2792d84a3cc4d5a8
Theerthananda <theerthanandagowda@gmail.com>
2026-03-04 22:35:18 +0530

    ADDED CIRCULAR QUEUE

---
+++ SORTING/SimpleCircularQueue.c
@@ -0,0 +1,79 @@
+/*
+ * @author Theerthananda
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#define N 5
+
+int q[N];
+int front=-1,rear=-1;
+
+void Enqueue(){
+	int x;
+	if((rear+1)%N==front){
+		printf("Queue is full !\n");
+		return;
+	}
+	
+	printf("\nEnter value to insert :");
+	scanf("%d",&x);
+	
+	if(front==-1&&rear==-1){
+		front=rear=0;
+		q[rear]=x;
+	}
+	
+	else{
+		rear=(rear+1)%N;
+		q[rear]=x;
+	}
+	
+}
+
+void Dequeue(){
+	if(front==-1&&rear==-1){
+		printf("Empty Queue !\n");
+		return;
+	}
+	
+	if(front==rear){
+		printf("Deleted element: %d\n", q[front]);
+		front=rear=-1;
+		
+	}
+	
+	else{
+		printf("Deleted element: %d\n", q[front]);
+		front=(front+1)%N;
+	}
+}
+
+void Display(){
+	if(front==-1&&rear==-1){
+		printf("Empty Queue !\n");
+		return;
+	}
+	
+	printf("Queue Elemnts :\n");
+	
+	int i=front;
+	while(1){
+		printf("%d \n",q[i]);
+		if(i==rear)
+		break;
+		i=(i+1)%N;
+	}
+}
+
+int main(){
+	Enqueue();
+	Enqueue();
+	Enqueue();
+	Enqueue();
+	Enqueue();
+	Enqueue();
+	Dequeue();
+	Display();
+}
+
