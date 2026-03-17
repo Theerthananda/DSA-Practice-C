a81eda70b9598ab81b7a7ff0bebb540fb37f3a7d
Theerthananda <theerthanandagowda@gmail.com>
2026-03-17 22:08:07 +0530

    pointer types

---
+++ POINTERS CONCEPTS/PointerTypes.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int *p=&a;
+    a=200;
+    printf("Adress of p: %d and value of p : %d\n",p,*p);
+    //printf("Adress of p+1: %d and value of p : %d\n",p+1,*(p+1));
+   /* unsigned char *p0;
+    p0=(unsigned char*)p; //typecasting
+
+    printf("Adress %d and value %d \n",p0,*p0);
+    printf("Adress %d and value %d \n",p0+1,*(p0+1));*/
+
+    void *p1;
+    p1=p;
+
+    printf("Adress : %d  ",p1+1);
+
+}
