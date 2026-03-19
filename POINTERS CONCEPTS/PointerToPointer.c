9c853cfe9550636b280d9d08a4abe0ca288e4744
Theerthananda <theerthanandagowda@gmail.com>
2026-03-19 21:30:41 +0530

    pointer to pointer

--- POINTERS CONCEPTS/PointerToPointer.c
+++ POINTERS CONCEPTS/PointerToPointer.c
@@ -7,7 +7,15 @@ int main(){
     *p=5;
     int **q;
     q=&p;
-    printf("%p\n",p);
-    printf("adress of p is %d\n", &p);
-    printf("adress of p stored in q is %d\n",**q);
+    int ***r;
+    r=&q;
+
+    printf("%d\n",*p);
+    printf("%d\n",*q);
+    printf("%d\n",**q);
+    printf("%d\n",*r);
+    printf("%d\n",**r);
+    printf("%d\n",***r);
+
+   
 }
